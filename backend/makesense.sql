9d7be34308e13966e4071ee43dec1839cbffed88
AlexandrePre <pre.alexandre1@gmail.com>
2022-12-15 09:58:32 +0100

    mvc user avec une route get

---
+++ backend/makesense.sql
@@ -0,0 +1,119 @@
+-- Active: 1669885952503@@127.0.0.1@3306@makesense
+​
+CREATE TABLE
+    `Status` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `status` varchar(255) NOT NULL,
+        PRIMARY KEY (`id`)
+    );
+​
+CREATE TABLE
+    `Firstdecision` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `content` varchar(255) NOT NULL,
+        PRIMARY KEY (`id`)
+    );
+​
+CREATE TABLE
+    `Finaldecision` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `content` varchar(255) NOT NULL,
+        PRIMARY KEY (`id`)
+    );
+​
+CREATE TABLE
+    `User` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `lastname` varchar(100) NOT NULL,
+        `firstname` varchar(100) NOT NULL,
+        `email` varchar(100) NOT NULL,
+        `is_admin` BOOLEAN NOT NULL,
+        PRIMARY KEY (`id`)
+    );
+​
+CREATE TABLE
+    `Decision` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `title` varchar(100) NOT NULL,
+        `content` varchar(255) NOT NULL,
+        `deadline` DATE NOT NULL,
+        `contexte` varchar(255) NOT NULL,
+        `profit` varchar(255) NOT NULL,
+        `usefullness` varchar(255) NOT NULL,
+        `inconvenience` varchar(255) NOT NULL,
+        `id_user` INT NOT NULL,
+        `date_posted` DATE NOT NULL,
+        `id_status` INT NOT NULL,
+        `id_firstdecision` INT NULL,
+        `id_finaldecision` INT NULL,
+        PRIMARY KEY (`id`)
+    );
+​
+CREATE TABLE
+    `Authorization` (
+        `id` INT NOT NULL AUTO_INCREMENT, 
+        `id_user` INT NOT NULL,
+        `id_decision` INT NOT NULL,
+        `is_expert` BOOLEAN NOT NULL, 
+        PRIMARY KEY (`id`) 
+    );
+​
+CREATE TABLE
+    `Notice` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `id_decision` INT NOT NULL,
+        `id_user` INT NOT NULL,
+        `content` varchar(255) NOT NULL,
+        `date` DATE NOT NULL,
+        PRIMARY KEY (`id`)
+    );
+​
+CREATE TABLE
+    `Conflict` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `id_decision` INT NOT NULL,
+        `id_user` INT NOT NULL,
+        `content` varchar(255) NOT NULL,
+        `date` DATE NOT NULL,
+        PRIMARY KEY (`id`)
+    );
+​
+ALTER TABLE `Decision`
+ADD
+    CONSTRAINT `fk_Decision_id_user` FOREIGN KEY(`id_user`) REFERENCES `User` (`id`);
+​
+ALTER TABLE `Decision`
+ADD
+    CONSTRAINT `fk_Decision_id_status` FOREIGN KEY(`id_status`) REFERENCES `Status` (`id`);
+​
+ALTER TABLE `Decision`
+ADD
+    CONSTRAINT `fk_Decision_id_firstdecision` FOREIGN KEY(`id_firstdecision`) REFERENCES `Firstdecision` (`id`);
+​
+ALTER TABLE `Decision`
+ADD
+    CONSTRAINT `fk_Decision_id_finaldecision` FOREIGN KEY(`id_finaldecision`) REFERENCES `Finaldecision` (`id`);
+​
+ALTER TABLE `Authorization`
+ADD
+    CONSTRAINT `fk_Authorization_id_user` FOREIGN KEY(`id_user`) REFERENCES `User` (`id`);
+​
+ALTER TABLE `Authorization`
+ADD
+    CONSTRAINT `fk_Authorization_id_decision` FOREIGN KEY(`id_decision`) REFERENCES `Decision` (`id`);
+​
+ALTER TABLE `Notice`
+ADD
+    CONSTRAINT `fk_Notice_id_decision` FOREIGN KEY(`id_decision`) REFERENCES `Decision` (`id`);
+​
+ALTER TABLE `Notice`
+ADD
+    CONSTRAINT `fk_Notice_id_user` FOREIGN KEY(`id_user`) REFERENCES `User` (`id`);
+​
+ALTER TABLE `Conflict`
+ADD
+    CONSTRAINT `fk_Conflict_id_decision` FOREIGN KEY(`id_decision`) REFERENCES `Decision` (`id`);
+​
+ALTER TABLE `Conflict`
+ADD
+    CONSTRAINT `fk_Conflict_id_user` FOREIGN KEY(`id_user`) REFERENCES `User` (`id`);
