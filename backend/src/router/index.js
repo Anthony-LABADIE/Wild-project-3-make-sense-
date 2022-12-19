ae15ace2405275d4b781b185e04873c6c29c7123
mehdy <mehdy33852@gmail.com>
2022-12-19 10:31:52 +0100

    push backend conflict

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -3,12 +3,10 @@ const routerDecision = require("./routerDecision");
 const userRouter = require("./userRouter");
 const conflictRouter = require("./conflictRouter");
 
-
 const router = express.Router();
 
 router.use("/decision", routerDecision);
 router.use("/user", userRouter);
 router.use("/conflict", conflictRouter);
-
 
 module.exports = router;
