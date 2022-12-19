2faf3d86bf53ba67e7308706a5da580b4c96dd18
AlexandrePre <pre.alexandre1@gmail.com>
2022-12-19 13:20:06 +0100

    avec table first decision et uptade user et hashing password sur update

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routerDecision = require("./routerDecision");
+const routerFirstDecision = require("./routerFirstDecision");
 const userRouter = require("./userRouter");
 
 const router = express.Router();
 
 router.use("/decision", routerDecision);
 router.use("/user", userRouter);
+router.use("/firstdecsion", routerFirstDecision);
 
 module.exports = router;
