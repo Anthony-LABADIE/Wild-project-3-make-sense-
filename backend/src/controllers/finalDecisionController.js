36bb6c050f6a2aad55306d0c8375675b53f328cc
mehdy <mehdy33852@gmail.com>
2022-12-19 12:03:20 +0100

    pull dev

---
+++ backend/src/controllers/finalDecisionController.js
@@ -0,0 +1,75 @@
+/* eslint-disable camelcase  */
+// (voir avec David pour le camelcase ..)
+const finalDecisionModel = require("../models/finalDecisionModel");
+
+const finalDecisionController = {
+  getAllFinalDecision: (_, res) => {
+    finalDecisionModel
+      .findAll()
+      .then((decision) => res.send(decision))
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  },
+  getOneFinalDecision: (req, res) => {
+    const { id } = req.params;
+    finalDecisionModel
+      .findOne(id)
+      .then((decision) => {
+        if (decision.length === 0) {
+          res.sendStatus(404);
+        } else {
+          res.send(decision[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  },
+  postFinalDecision: (req, res) => {
+    const { content } = req.body;
+    finalDecisionModel
+      .createOne({
+        content,
+      })
+      .then((result) =>
+        res.status(201).send({
+          id: result.insertId,
+          content,
+        })
+      )
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  },
+  deleteFinalDecision: (req, res) => {
+    const { id } = req.params;
+    finalDecisionModel
+      .deleteOne(id)
+      .then((response) => {
+        if (response.affectedRows !== 1) {
+          return res.status(404).send(`decision ${id} not found`);
+        }
+        return res.status(200).send(`decision ${id} deleted`);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  },
+  updateFinalDecision: (req, res) => {
+    const { id } = req.params;
+    const decisionData = req.body;
+    finalDecisionModel
+      .updateOne(decisionData, id)
+      .then((decision) => res.send(decision))
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  },
+};
+module.exports = finalDecisionController;
--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const routerDecision = require("./routerDecision");
 const userRouter = require("./userRouter");
 const conflictRouter = require("./conflictRouter");
+const finalDecisionRouter = require("./finalDecisionRouter");
 
 const router = express.Router();
 
 router.use("/decision", routerDecision);
 router.use("/user", userRouter);
 router.use("/conflict", conflictRouter);
+router.use("/finalDecision", finalDecisionRouter);
 
 module.exports = router;
