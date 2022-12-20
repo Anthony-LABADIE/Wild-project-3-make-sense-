ad4fce8ec1333728f5c421b738fadce816929388
Nicolas Baudet <[baudetnicolas@hotmail.fr](mailto:baudetnicolas@hotmail.fr)>
2022-12-20 10:05:27 +0100

    notice with by email ok

---
+++ backend/src/controllers/noticeController.js
@@ -0,0 +1,66 @@
+/* eslint-disable camelcase  */
+
+const noticeModel = require("../models/noticeModel");
+
+const noticeController = {
+  getAllNotice: (req, res) => {
+    noticeModel
+      .findAllnotice()
+      .then((notice) => res.send(notice))
+      .catch((err) => res.send(err));
+  },
+
+  getOneNotice: (req, res) => {
+    const { id } = req.params;
+    noticeModel
+      .findOneNotice(id)
+      .then((notice) => {
+        if (notice.length === 0) {
+          res.sendStatus(404);
+        } else {
+          res.send(notice[0]);
+        }
+      })
+      .catch((err) => res.send(err));
+  },
+
+  getOneNoticeByEmail: (req, res) => {
+    const { email } = req.query;
+    noticeModel
+      .FindOneNoticeByEmail(email)
+      .then((notice) => res.send(notice))
+      .catch((err) => res.send(err));
+  },
+
+  createOneNotice: (req, res) => {
+    const { id_decision, id_user, content, date } = req.body;
+    noticeModel
+      .createOneNotice({ id_decision, id_user, content, date })
+      .then((notice) => res.send(notice))
+      .catch((err) => res.send(err));
+  },
+
+  modifyNotice: (req, res) => {
+    const { id } = req.params;
+    const noticeData = req.body;
+    noticeModel
+      .modifyNotice({ noticeData, id })
+      .then((notice) => res.send(notice))
+      .catch((err) => res.send(err));
+  },
+
+  deleteNotice: (req, res) => {
+    const { id } = req.params;
+    noticeModel
+      .deleteOneNotice(id)
+      .then((notice) => {
+        if (notice.affectedRows !== 1) {
+          return res.status(404).send(`notice ${id} not found`);
+        }
+        return res.status(200).send(`notice ${id} deleted`);
+      })
+      .catch((err) => res.send(err));
+  },
+};
+
+module.exports = noticeController;
---
+++ backend/src/models/noticeModel.js
@@ -0,0 +1,60 @@
+/* eslint-disable camelcase  */
+
+const db = require("../../config");
+
+const findAllnotice = () => {
+  return db
+    .promise()
+    .query("SELECT * FROM notice")
+    .then(([res]) => res);
+};
+
+const findOneNotice = (id) => {
+  return db
+    .promise()
+    .query("SELECT * FROM notice WHERE id = ?", [Number(id)])
+    .then(([res]) => res);
+};
+
+const FindOneNoticeByEmail = (email) => {
+  return db
+    .promise()
+    .query(
+      "SELECT * FROM notice LEFT JOIN user ON notice.id_user = user.id WHERE email = ?",
+      [email]
+    )
+    .then(([res]) => res);
+};
+
+const createOneNotice = ({ id_decision, id_user, content, date }) => {
+  return db
+    .promise()
+    .query(
+      "INSERT INTO notice (id_decision,id_user,content,date) VALUES (?,?,?,?)",
+      [id_decision, id_user, content, date]
+    )
+    .then(([res]) => res);
+};
+
+const modifyNotice = ({ noticeData, id }) => {
+  return db
+    .promise()
+    .query("UPDATE notice SET ? WHERE id = ?", [noticeData, id])
+    .then(([res]) => res);
+};
+
+const deleteOneNotice = (id) => {
+  return db
+    .promise()
+    .query("DELETE FROM notice WHERE id = ?", [id])
+    .then(([res]) => res);
+};
+
+module.exports = {
+  findAllnotice,
+  findOneNotice,
+  createOneNotice,
+  modifyNotice,
+  deleteOneNotice,
+  FindOneNoticeByEmail,
+};
