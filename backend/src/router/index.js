ac6f4b0c65cdfca3d3b8463d1ddc0ff80e6191f0
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-12-20 12:06:09 +0100

    Add route decision by user

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const routerDecision = require("./routerDecision");
 const userRouter = require("./userRouter");
 const statusRouter = require("./routerStatus");
+const UserDecisionRouter = require("./routerDecicionUser");
 
 const router = express.Router();
 
 router.use("/decision", routerDecision);
 router.use("/user", userRouter);
 router.use("/status", statusRouter);
+router.use("/user/decision", UserDecisionRouter);
 
 module.exports = router;
