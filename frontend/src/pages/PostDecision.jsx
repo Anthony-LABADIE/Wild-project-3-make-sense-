e34835e450c28172c0973a9330336b61e24afb12
AlexandrePre <pre.alexandre1@gmail.com>
2023-01-18 09:10:37 +0100

    avant pull dev

--- frontend/src/pages/PostDecision.jsx
+++ frontend/src/pages/PostDecision.jsx
@@ -1,11 +1,9 @@
-import NavDec from "../components/postDecision/NavDec";
 import NavBar from "../components/dashboard/NavBardash";
 
 export default function PostDecision() {
   return (
     <div>
       <NavBar />
-      <NavDec />
       <p> je suis là </p>
     </div>
   );
