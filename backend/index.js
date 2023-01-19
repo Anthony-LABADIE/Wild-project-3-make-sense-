a13df4f2678079067b3d80aec94d6a34a657842e
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2023-01-19 11:50:46 +0100

    Add socket io in BACK

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
 require("dotenv").config();
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = require("./src/app");
 
+const Server = http.createServer(app);
+
 const port = parseInt(process.env.APP_PORT ?? "5000", 10);
 
-app.listen(port, (err) => {
+const io = socketIo(Server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+});
+io.on("connection", (socket) => {
+  // eslint-disable-next-line no-restricted-syntax
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    // eslint-disable-next-line no-restricted-syntax
+    console.log("🔥: A user disconnected");
+  });
+});
+
+Server.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
