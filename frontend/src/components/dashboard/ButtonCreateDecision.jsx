cd10205ef30812c85f50f7d79c62e958b7f980fe
AlexandrePre <pre.alexandre1@gmail.com>
2023-01-20 11:05:29 +0100

    avant pull medhi

--- frontend/src/components/dashboard/ButtonCreateDecision.jsx
+++ frontend/src/components/dashboard/ButtonCreateDecision.jsx
@@ -5,8 +5,8 @@ import "./ButtonCreateDecision.css";
 export default function ButtonCreateDecision() {
   return (
     <div className="btn-decision">
-      {/* <Link to="/decision"> */}
-      <Link to="/notice">
+      <Link to="/decision">
+        {/* <Link to="/notice"> */}
         <button className="btn-decisioncreate" type="button">
           Créer une décision
         </button>
